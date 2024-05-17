8164b0fe69614b9f7b837f05ad04edb5ca925cd4
AmazingAaravCoding <142331565+AmazingAaravCoding@users.noreply.github.com>
2024-05-17 17:59:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 Webcam.set({
-    width:350,
-    height:300,
-    image_format:"png",
-    png_quality:90
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
 })
-camera=document.getElementById("camera")
+camera = document.getElementById("camera")
 Webcam.attach("#camera")
-function capture(){
-Webcam.snap(function(data){
-    document.getElementById("result").innerHTML="<img id=picture src="+data+">"
-})
+function capture() {
+    Webcam.snap(function (data) {
+        document.getElementById("result").innerHTML = "<img id=picture src=" + data + ">"
+    })
 }
-function identify(){
-    
+function identify() {
+    img = document.getElementById("picture")
+    classifier.classify(img, gotResult)
 }
 console.log(ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/jsXcXYQux/model.json",modelLoaded)
-function modelLoaded(){
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/jsXcXYQux/model.json", modelLoaded)
+function modelLoaded() {
     console.log("model is loaded");
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("resultobject").innerHTML=results[0].label
+        document.getElementById("resultaccuracy").innerHTML=results[0].confidence.toFixed(3)
+    }
 }
